bb6a228552c91a31bd89bebe8b34040ea34eb1d8
BLBaylis <bradleybaylis@hotmail.com>
2020-04-02 22:11:37 +0100

    Display book titles from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import bookService from './services/books'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    books: []
+  }
+
+  getBooksData = async () => {
+    const books = await bookService.getAll()
+    this.setState({ books })
+  }
+
+  componentDidMount() {
+    this.getBooksData()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {this.state.books.map(book => <p key = {book.id}>{book.title}</p>)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
