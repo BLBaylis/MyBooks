154637d90e3d628e6e7e635918ca35edd8c016bb
BLBaylis <bradleybaylis@hotmail.com>
2020-04-03 21:45:38 +0100

    Remove openSearch from prop types

--- src/views/Library.js
+++ src/views/Library.js
@@ -6,29 +6,29 @@ import BookShelf from '../components/BookShelf'
 class Library extends Component {
     
     render() {
-        return (
-            <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <BookShelf name = "Currently Reading" books = {this.props.books} />
-                <BookShelf name = "Want to Read" books = {this.props.books} />
-                <BookShelf name = "Read" books = {this.props.books} />
-              </div>
-            </div>
-            <div className="open-search">
-                <Link className = "fab-link" to = "/search">Add a book</Link>
+      const books = this.props.books
+      return (
+          <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              <BookShelf name = "Currently Reading" books = {books} />
+              <BookShelf name = "Want to Read" books = {books} />
+              <BookShelf name = "Read" books = {books} />
             </div>
           </div>
-        )
+          <div className="open-search">
+              <Link className = "fab-link" to = "/search">Add a book</Link>
+          </div>
+        </div>
+      )
     }
 }
 
 Library.propTypes = {
-    books: PropTypes.arrayOf(PropTypes.object).isRequired,
-    openSearch: PropTypes.func.isRequired
+  books: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
 export default Library
